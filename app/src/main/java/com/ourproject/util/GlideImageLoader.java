750d24e49148d867223d1e125b05cc49470d1d43
WHaiNan <rj_whn@163.com>
2017-04-10 18:57:25 +0800

    api

---
+++ app/src/main/java/com/ourproject/util/GlideImageLoader.java
@@ -0,0 +1,18 @@
+package com.ourproject.util;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by admin on 2017/4/10.
+ */
+
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load((String) path).into(imageView);
+    }
+}
